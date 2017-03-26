1ca4b68247e8ee5e065ab43bc8c5babae5564077
bbbxyz <bbbxyz@users.noreply.github.com>
2017-03-26 00:37:41 -0400

    Fixes

--- tf2.py
+++ tf2.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 import numpy as np
 from random import shuffle
 from time import time
-from Queue import Queue
+from queue import Queue
 import pandas as pd
 from functools import reduce
 from sklearn.model_selection import train_test_split
@@ -20,13 +20,13 @@ y_col = -2 #-3: temp, -2: energy, -1: magnetization
 batch_size = 10 #number of samples from each file to sample when building batches
 batch_sampling_size = 100
 batch_per_epoch = 5
-split_test = 0.005
-learning_rate = 1e-2 #learning rate for gradient descent
+split_test = 0.1
+learning_rate = 1e-3 #learning rate for gradient descent
 epsilon = 0.05  #error at which to stop training
 l2_alpha = 0.000
 dim = cst.lattice_size
 data_type = tf.float32
-batches = Queue(maxsize=10)
+batches = Queue(maxsize=20)
 
 #split for train/test
 files = glob.glob('data/*.csv')
@@ -38,21 +38,21 @@ data_type = tf.float32
 x = tf.placeholder(tf.float32, [None, feat])
 y_ = tf.placeholder(tf.float32, [None,1])
 
-n_filters=      [ 4,  4]
-filter_sizes=   [ 3,  3]
-pool =          [ 0,  0]
+n_filters=      [32]
+filter_sizes=   [ 3]
+pool =          [ 1]
 
 #helper functions
 def kernel_variable(shape):
-  initial = tf.truncated_normal(shape, mean=0, stddev=0.1, dtype = data_type)
+  initial = tf.truncated_normal(shape, mean=0, stddev=0.5, dtype = data_type)
   return tf.Variable(initial)
   
 def weight_variable(shape):
-  initial = tf.truncated_normal(shape, mean=0, stddev=0.1, dtype = data_type)
+  initial = tf.truncated_normal(shape, mean=0, stddev=0.001/float(shape[1]), dtype = data_type)
   return tf.Variable(initial)
 
 def bias_variable(shape):
-  initial = tf.constant(0.1, shape=shape, dtype = data_type)
+  initial = tf.constant(0.0, shape=shape, dtype = data_type)
   return tf.Variable(initial)
 
 def conv2d(x, W):
@@ -76,35 +76,35 @@ for i, n_output in enumerate(n_filters):
             filter_sizes[i],
             n_input, n_output])
     b = bias_variable([n_output])
-    output =  tf.nn.relu(tf.add(conv2d(current_input, k), b))
+    output =  tf.add(conv2d(current_input, k), b)
     if(pool[i]):
         output = avg_pool_2x2(output)
-    current_input = tf.nn.relu(output)
+    current_input = tf.tanh(output)
 
 conv_output_size = reduce(lambda x, y: x*y, current_input.get_shape().as_list() [1:])
-conv_output_flat = tf.reshape(current_input, [-1, conv_output_size])
+conv_output_flat = (tf.reshape(current_input, [-1, conv_output_size]))
 print(conv_output_size)
-fc1size = conv_output_size
-fc2size = conv_output_size/10
+fc1size = 10
+fc2size = 10
 
 W_fc1 = weight_variable([conv_output_size, fc1size])
 b_fc1 = bias_variable([fc1size])
 
-W_fc2 = weight_variable([fc1size,fc2size ])
+W_fc2 = weight_variable([fc1size, fc2size])
 b_fc2 = bias_variable([fc2size])
 
-W_o = weight_variable([fc2size, 1])
+W_o = weight_variable([conv_output_size, 1])
 b_o= bias_variable([1])
 
 keep_prob = tf.placeholder(data_type)
-h_fc1 = tf.nn.relu(tf.matmul(conv_output_flat, W_fc1) + b_fc1)
-h_fc2 = tf.matmul(h_fc1, W_fc2) + b_fc2
-y = tf.matmul(h_fc2, W_o) + b_o
+
+
+y = tf.matmul(conv_output_flat, W_o) + b_o
 
 #l2_loss = l2_alpha*( tf.nn.l2_loss(W_fc2) + tf.nn.l2_loss(W_fc1) + tf.nn.l2_loss(W_o) ) 
 loss =  tf.reduce_mean(tf.square(y-y_))
-accuracy = tf.reduce_mean(abs((y-y_)/y_)) 
-train_step = tf.train.AdadeltaOptimizer(learning_rate).minimize(loss)
+accuracy = 100*tf.reduce_mean(abs((y-y_)/y_)) 
+train_step = tf.train.AdagradOptimizer(learning_rate).minimize(loss)
 
 saver = tf.train.Saver()
 
@@ -136,13 +136,13 @@ def get_normalization_params():
     return mean,stddev
 
 def train_set(trainX, trainY):
-    a, score = sess.run((train_step, accuracy),\
+    a, lo = sess.run((train_step, accuracy),\
         feed_dict={x: trainX, y_: trainY, keep_prob: 0.5})
-    return score
+    return lo
 
 def test_set(testX, testY):
-    score, lo, predic, real = sess.run((accuracy,loss,  y, y_), feed_dict={x: testX , y_: testY, keep_prob: 1.0 })
-    print(predic[-1],real[-1])
+    score, predic, real = sess.run((accuracy,  y, y_), feed_dict={x: testX , y_: testY, keep_prob: 1.0 })
+    #print(predic[-1],real[-1])
     #print(lo)
     return score
  
@@ -156,7 +156,7 @@ def calculate_score():
       X = df.values[:, :-3]
 
       #scale X to [0,1]
-      X = (X+1.0)/2.0
+      #X = (X+1.0)/2.0
 
       #Normalize y
       Y = (Y-mean)/float(stddev) 
@@ -174,6 +174,7 @@ def create_batches():
         minibatchY = np.array([]).reshape(0,1)
         sel_f = np.random.random_integers(0,len(train)-1,batch_sampling_size).tolist()
         samples = [train[i] for i in sel_f]
+        
         for file in samples:
           #print(file)
           df=pd.read_csv(file)
@@ -183,33 +184,37 @@ def create_batches():
           sel = np.random.random_integers(0,len(Y)-1, batch_size)
           
           #scale X to [0,1]
-          X = (X[sel]+1.0)/2.0
+          #X = (X[sel]+1.0)/2.0
+          X = X[sel]
           minibatchX = np.vstack((minibatchX,X))
           
           #normalize Y
           Y = (Y[sel]-mean)/float(stddev)
+          #Y = (Y-mean)/float(stddev)
           minibatchY = np.vstack((minibatchY,Y))
-        batches.put((minibatchX, minibatchY))
+          #batches.put((X,Y))
+        batches.put((minibatchX,minibatchY))
 
 def train_dataset():
     k = 0 #counter to keep track of number of times we've trained on the entire set 
     sc = 1.0 
     while(sc>epsilon): 
+        train_err = 0.0
         t0=time()
         for j in range(batch_per_epoch):
           minibatchX, minibatchY = get_batch()
-          train_err = train_set(minibatchX, minibatchY)
+          train_err += train_set(minibatchX, minibatchY)
         t2=time()    
-        sc = calculate_score()
+        test_err = calculate_score()
         t3=time()
-        print(train_err)
-        print("Error: %f, Training time: %is, Test time: %is,\
-        Epoch %i" % (sc, (t2-t0), (t3-t2), k),end='\n')
+        train_err = train_err/float(batch_per_epoch)
+        print("Train/Test Error: %f/%f, Train/Test Time: %is/%is\
+        Epoch %i" % (train_err, test_err, (t2-t0), (t3-t2), k),end='\n')
         k += 1
 
 
 print("Calculating normalization parameters")
-#shuffle(train)
+shuffle(train)
 mean,stddev = get_normalization_params()
 print("Done")
 print("Creating threads")
@@ -220,8 +225,7 @@ creator_thread1 = threading.Thread(target=create_batches)
 trainer_thread= threading.Thread(target = train_dataset)
 print("Done")
 
-#start training until test set error is smaller than threshold 
-print("Starting training\n")
+print("Starting training")
 creator_thread1.start()
 #creator_thread2.start()
 #creator_thread3.start()
