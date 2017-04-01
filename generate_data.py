de513325bd7f46fbb83630ced07bd8600f88186b
mass <enissam_426@hotmail.com>
2017-04-01 02:13:48 -0400

    fix

--- generate_data.py
+++ generate_data.py
@@ -15,7 +15,7 @@ t0 = time.time()
 for i in cst.temps:
     for nid in range(cst.instances):
         print(i, nid)
-        ising = im.IsingMonteCarlo(cst.lattice_size, i, cst.iterations, nid)
-ising.run()
+        ising = im.IsingMonteCarlo(cst.lattice_size, i, cst.iterations, nid+1)
+		ising.run()
 t1 = time.time()
 print("Data generated. Time taken: %is" % (t1-t0))
